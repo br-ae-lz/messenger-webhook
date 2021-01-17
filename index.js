e9cd139bca37c3b0234c87e632cd6311a9129e48
Braden <bradenschulz@gmail.com>
2021-01-17 06:56:27 -0500

    juuust wanna see talking on the screen

--- index.js
+++ index.js
@@ -154,4 +154,18 @@ function callSendAPI(sender_psid, response) {
     },
     "message": response
   }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
 }
