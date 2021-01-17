5a2ab30b957ab196c98e4b1623fe09f7bed72af3
Braden <bradenschulz@gmail.com>
2021-01-17 03:44:08 -0500

    work now?? ??? ????

--- index.js
+++ index.js
@@ -74,6 +74,34 @@ app.get('/webhook', (req, res) => {
     
 });
 
+app.post('/webhook', (req, res) => {  
+
+  // Parse the request body from the POST
+  let body = req.body;
+
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Get the webhook event. entry.messaging is an array, but 
+      // will only ever contain one event, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+      
+    });
+
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
+
 if (webhook_event.message) {
   callSendAPI("woah cool");
 }
