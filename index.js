e478d0853048c18e2aab679e52ac283cfe9a6fe7
Braden <bradenschulz@gmail.com>
2021-01-17 06:38:55 -0500

    so close. so close.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const
   express = require('express'),
   // bodyParser = require('body-parser'),
   app = express(); //Used to parse JSON bodies
+  request = require('request');
 
 var path = require('path');
 
@@ -12,11 +13,28 @@ app.use(express.json()); //Used to parse JSON bodies
 
 app.use(express.static("public"));
 
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
 
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {
 
   let body = req.body;
+
+  console.log(":)))))))))))))))))))))))))");
+  console.log(body);
 
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
@@ -36,6 +54,39 @@ app.post('/webhook', (req, res) => {
     // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
+
+  body.entry.forEach(function(entry) {
+
+    // Gets the body of the webhook event
+    let webhook_event = entry.messaging[0];
+    console.log(webhook_event);
+  
+    // Get the sender PSID
+    let sender_psid = webhook_event.sender.id;
+    console.log('Sender PSID: ' + sender_psid);
+  
+  });
+
+  body.entry.forEach(function(entry) {
+
+    // Gets the body of the webhook event
+    let webhook_event = entry.messaging[0];
+    console.log(webhook_event);
+  
+  
+    // Get the sender PSID
+    let sender_psid = webhook_event.sender.id;
+    console.log('Sender PSID: ' + sender_psid);
+  
+    // Check if the event is a message or postback and
+    // pass the event to the appropriate handler function
+    if (webhook_event.message) {
+      handleMessage(sender_psid, webhook_event.message);        
+    } else if (webhook_event.postback) {
+      handlePostback(sender_psid, webhook_event.postback);
+    }
+    
+  });
 
 });
 
@@ -77,3 +128,31 @@ app.get('/webhook', (req, res) => {
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 5000, () => console.log('webhook is listening'));
+
+
+function handleMessage(sender_psid, received_message) {
+
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {    
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }  
+  
+  // Sends the response message
+  callSendAPI(sender_psid, response);    
+}
+
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+}
