146c1d2b1ce4adabc9910a0c6d7a77b117f8594e
Braden <bradenschulz@gmail.com>
2021-01-17 05:18:23 -0500

    make the bot be a bot

--- index.js
+++ index.js
@@ -35,51 +35,51 @@ app.post('/webhook', (req, res) => {
       // Returns a '404 Not Found' if event is not from a page subscription
       res.sendStatus(404);
     }
+
+    if (webhook_event.message) {
+      callSendAPI("woah cool");
+    }
   
   });
 
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/maps.html'));
+
+  // Adds support for GET requests to our webhook
+  app.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
+    
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
+  else {
+      res.sendStatus(403);
+  }
 });
 
-/*
-// Adds support for GET requests to our webhook
-app.get('/webhook', (req, res) => {
 
-    // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
-      
-    // Parse the query params
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-      
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
-    
-      // Checks the mode and token sent is correct
-      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-        
-        // Responds with the challenge token from the request
-        console.log('WEBHOOK_VERIFIED');
-        res.status(200).send(challenge);
-      
-      } else {
-        // Responds with '403 Forbidden' if verify tokens do not match
-        res.sendStatus(403);      
-      }
-    }
-    else {
-        res.sendStatus(403);
-    }
-    
-});
 
-let webhook_event = entry.messaging[0];
-if (webhook_event.message) {
-  callSendAPI("woah cool");
-}
+
   
 function callSendAPI(sender_psid, response) {
   // Construct the message body
@@ -90,4 +90,3 @@ function callSendAPI(sender_psid, response) {
     "message": response
   }
 }
-*/
