7dfa6f9c9f67f3acb904ec7c8a5ab2a088508b0d
Braden <bradenschulz@gmail.com>
2021-01-17 07:31:19 -0500

    :(( buttons

--- index.js
+++ index.js
@@ -61,8 +61,6 @@ app.post('/webhook', (req, res) => {
       handlePostback(sender_psid, webhook_event.postback);
     }
 
-    handleMessage(sender_psid, webhook_event.message);
-
 });
 
 app.get('/', function (req, res) {
@@ -164,12 +162,7 @@ function handlePostback(sender_psid, received_postback) {
 
 function handleMessage(sender_psid, received_message) {
   let response;
-  
-  // Checks if the message contains text
-  if (received_message.text) {    
-    // Create the payload for a basic text message, which
-    // will be added to the body of our request to the Send API
-    let attachment_url = received_message.attachments[0].payload.url;
+   
     response = {
       "attachment": {
         "type": "template",
