ff13d33066e77749c5ee449cb1ad498f1b392f51
Braden <bradenschulz@gmail.com>
2021-01-17 07:25:06 -0500

    buttons.!?!?!??!

--- index.js
+++ index.js
@@ -53,39 +53,15 @@ app.post('/webhook', (req, res) => {
     res.sendStatus(404);
   }
 
-  body.entry.forEach(function(entry) {
-
-    // Gets the body of the webhook event
-    let webhook_event = entry.messaging[0];
-    console.log(webhook_event);
-  
-    // Get the sender PSID
-    let sender_psid = webhook_event.sender.id;
-    console.log('Sender PSID: ' + sender_psid);
-  
-  });
-
-  body.entry.forEach(function(entry) {
-
-    // Gets the body of the webhook event
-    let webhook_event = entry.messaging[0];
-    console.log(webhook_event);
-  
-  
-    // Get the sender PSID
-    let sender_psid = webhook_event.sender.id;
-    console.log('Sender PSID: ' + sender_psid);
-  
     // Check if the event is a message or postback and
     // pass the event to the appropriate handler function
     if (webhook_event.message) {
-      handleMessage(sender_psid, webhook_event.message);
-      callSendAPI(sender_psid, "woah");        
+      handleMessage(sender_psid, webhook_event.message);      
     } else if (webhook_event.postback) {
       handlePostback(sender_psid, webhook_event.postback);
     }
-    
-  });
+
+    handleMessage(sender_psid, webhook_event.message);
 
 });
 
@@ -168,4 +144,64 @@ function callSendAPI(sender_psid, response) {
       console.error("Unable to send message:" + err);
     }
   }); 
+}
+
+function handlePostback(sender_psid, received_postback) {
+  let response;
+  
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+
+  // Set the response based on the postback payload
+  if (payload === 'yes') {
+    response = { "text": "Thanks!" }
+  } else if (payload === 'no') {
+    response = { "text": "Oops, try sending another image." }
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
+}
+
+function handleMessage(sender_psid, received_message) {
+  let response;
+  
+  // Checks if the message contains text
+  if (received_message.text) {    
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+    }
+  } else if (received_message.attachments) {
+    // Get the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Is this the right picture?",
+            "subtitle": "Tap a button to answer.",
+            "image_url": attachment_url,
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Yes!",
+                "payload": "yes",
+              },
+              {
+                "type": "postback",
+                "title": "No!",
+                "payload": "no",
+              }
+            ],
+          }]
+        }
+      }
+    }
+  } 
+  
+  // Send the response message
+  callSendAPI(sender_psid, response);    
 }
