cca5786567e3a5c1bf3b0d04975ef003bbf033b7
Braden <bradenschulz@gmail.com>
2021-01-17 07:28:39 -0500

    buttons...?

--- index.js
+++ index.js
@@ -169,11 +169,6 @@ function handleMessage(sender_psid, received_message) {
   if (received_message.text) {    
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
-    response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-    }
-  } else if (received_message.attachments) {
-    // Get the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
     response = {
       "attachment": {
@@ -183,7 +178,6 @@ function handleMessage(sender_psid, received_message) {
           "elements": [{
             "title": "Is this the right picture?",
             "subtitle": "Tap a button to answer.",
-            "image_url": attachment_url,
             "buttons": [
               {
                 "type": "postback",
