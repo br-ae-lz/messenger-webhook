0ca58b88216c553bef5dfcdb7a896cc68704ea01
Braden <bradenschulz@gmail.com>
2021-01-17 04:40:43 -0500

    testing messenger again

--- index.js
+++ index.js
@@ -38,10 +38,12 @@ app.post('/webhook', (req, res) => {
   
   });
 
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/maps.html'));
 });
 
+/*
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
 
@@ -74,34 +76,7 @@ app.get('/webhook', (req, res) => {
     
 });
 
-app.post('/webhook', (req, res) => {  
-
-  // Parse the request body from the POST
-  let body = req.body;
-
-  // Check the webhook event is from a Page subscription
-  if (body.object === 'page') {
-
-    // Iterate over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-
-      // Get the webhook event. entry.messaging is an array, but 
-      // will only ever contain one event, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-      
-    });
-
-    // Return a '200 OK' response to all events
-    res.status(200).send('EVENT_RECEIVED');
-
-  } else {
-    // Return a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-
-});
-
+let webhook_event = entry.messaging[0];
 if (webhook_event.message) {
   callSendAPI("woah cool");
 }
@@ -115,3 +90,4 @@ function callSendAPI(sender_psid, response) {
     "message": response
   }
 }
+*/
