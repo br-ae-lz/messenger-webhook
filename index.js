e955684e8d48fa78e52e0aa7d83f2068df67db7e
Braden Schulz <bradenschulz@gmail.com>
2023-08-17 00:25:22 -0400

    Refactor index.js, remove hardcoded page id in html

--- index.js
+++ index.js
@@ -1,50 +1,41 @@
 'use strict';
 
 // Imports dependencies and set up http server
-
+const dotenv = require('dotenv');
 const request = require('request');
 const
   express = require('express'),
   // bodyParser = require('body-parser'),
   app = express(); //Used to parse JSON bodies
 
-var path = require('path');
+const path = require('path');
 const { send } = require('process');
 
 let i = 0;
 let severity = 0;
 let count = 9;
+const titles = ["Do you feel fatigued?", "Are you experiencing headaches?", "Are you having difficulty speaking or moving?",
+"Do you have a diminished sense of taste or smell?", "Are you feverish?", "Do you have a sore throat?", 
+"Do you feel pain or pressure in your chest?", "Do you have a dry cough?", "Are you having difficulty breathing?"];
 
 app.use(express.json()); //Used to parse JSON bodies
 
 app.use(express.static("public"));
 
-// Handles messages events
-function handleMessage(sender_psid, received_message) {
+dotenv.config();
 
-}
+// Stub webhook functions (in case they need to be defined)
+function handleMessage(sender_psid, received_message) { }
+function handlePostback(sender_psid, received_postback) { }
 
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-
-}
-
-// Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
-  
-}
-
-// Creates the endpoint for our webhook 
+// Creates the endpoint for our webhook -- code and comments from messenger webhook example
 app.post('/webhook', (req, res) => {
-
   let body = req.body;
 
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
-
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function (entry) {
-
       // Gets the message. entry.messaging is an array, but 
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
@@ -52,7 +43,6 @@ app.post('/webhook', (req, res) => {
 
       let sender_psid = webhook_event.sender.id;
       console.log('Sender PSID: ' + sender_psid);
-
         
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
@@ -69,9 +59,6 @@ app.post('/webhook', (req, res) => {
     // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
-
-
-
 });
 
 app.get('/', function (req, res) {
@@ -80,7 +67,6 @@ app.get('/', function (req, res) {
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
-
   // Your verify token. Should be a random string.
   let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
 
@@ -91,14 +77,11 @@ app.get('/webhook', (req, res) => {
 
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
-
     // Checks the mode and token sent is correct
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
       // Responds with the challenge token from the request
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
-
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);
@@ -107,12 +90,10 @@ app.get('/webhook', (req, res) => {
   else {
     res.sendStatus(403);
   }
-
 });
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 5000, () => console.log('webhook is listening'));
-
+app.listen(process.env.PORT || 5000, () => console.log(`webhook is listening`));
 
 function callSendAPI(sender_psid, response) {
   // Construct the message body
@@ -138,25 +119,20 @@ function callSendAPI(sender_psid, response) {
   }); 
 }
 
+// Handle the user's responses to the chatbot
 function handlePostback(sender_psid, received_postback) {
   let response;
-  
   let payload = received_postback.payload;
 
-
+  // If the user has a symptom, increment severity appropriately
   if (payload === 'yes') {
-    // uncommon symptom
-    if (i == 1 || i == 3 || i == 5)
+    if (i == 1 || i == 3 || i == 5)         // uncommon symptom
       severity += 1;
-    // common symptom
-    else if (i == 0 || i == 4 || i == 7) 
+    else if (i == 0 || i == 4 || i == 7)    // common symptom
       severity += 2;
-    // severe symptom
-    else if (i == 2 || i == 6 || i == 8)
+    else if (i == 2 || i == 6 || i == 8)    // severe symptom
       severity += 4;
-  } else if (payload === 'no'){
-
-  }
+  } 
 
   if (i >= count){
     if (i <= 3){
@@ -183,11 +159,9 @@ function handlePostback(sender_psid, received_postback) {
   handleMessage(sender_psid, "")
 }
 
+
 function handleMessage(sender_psid, received_message) {
   let response;
-  let titles = ["Do you feel fatigued?", "Are you experiencing headaches?", "Are you having difficulty speaking or moving?",
-  "Do you have a diminished sense of taste or smell?", "Are you feverish?", "Do you have a sore throat?", 
-  "Do you feel pain or pressure in your chest?", "Do you have a dry cough?", "Are you having difficulty breathing?"];
   
   if (received_message == "" || i == 0){
     response = {
@@ -215,12 +189,10 @@ function handleMessage(sender_psid, received_message) {
       }
     } 
   }
-  else if (i == count){
+  else if (i == count) {
     response = {
       "text": `${received_message.text}`
     }
-  }else{
-
   }
   // Send the response message
   callSendAPI(sender_psid, response);    
